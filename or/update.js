34ed7793a22a450393dceb0e3d74fe282f850cfa
TheBoxCreeper <145577997+TheBoxCreeper@users.noreply.github.com>
2025-04-22 16:48:41 -0700

    Create update.js

---
+++ or/update.js
@@ -0,0 +1,4 @@
+const site = "https://sites.google.com/view/terminal-games";
+const drive = "https://drive.google.com/drive/folders/1Fc5m2RE5XyS05-HWORFb7ptZXnn5fAxd";
+const file = "https://drive.usercontent.google.com/u/0/uc?id=14KXNPDGLcU9Z4B2aGslmBmtdbw9JlD2j&export=download";
+const upbutton = 1;
