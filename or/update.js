5c9f1afe03e06a018eb80d183ebeac242bd4bb03
TheBoxCreeper <145577997+TheBoxCreeper@users.noreply.github.com>
2025-04-23 06:20:42 -0700

    Update update.js

--- or/update.js
+++ or/update.js
@@ -1,8 +1,9 @@
-const site = "https://sites.google.com/view/terminal-games";
+const site = "https://sites.google.com/view/terminal-games/downloads";
 const drive = "https://drive.google.com/drive/folders/1Fc5m2RE5XyS05-HWORFb7ptZXnn5fAxd";
 const file = "https://drive.usercontent.google.com/u/0/uc?id=14KXNPDGLcU9Z4B2aGslmBmtdbw9JlD2j&export=download";
 const upbutton = 1;
 const vdiv = document.getElementById("version");
 const version = vdiv.className;
 const latest = 8;
+document.getElementsById("TitleLink").setAttribute("href",site);
 console.log(version);
