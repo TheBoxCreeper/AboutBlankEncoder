b3a65ea12db672f250f59e84a4439cd5f151e73c
TheBoxCreeper <145577997+TheBoxCreeper@users.noreply.github.com>
2025-04-23 20:54:48 -0700

    Update update.js

--- or/update.js
+++ or/update.js
@@ -3,11 +3,12 @@ const drive = "https://drive.google.com/drive/folders/1Fc5m2RE5XyS05-HWORFb7ptZX
 const file = "https://drive.usercontent.google.com/u/0/uc?id=14KXNPDGLcU9Z4B2aGslmBmtdbw9JlD2j&export=download";
 const sheet = "https://docs.google.com/spreadsheets/u/0/d/1sQ9YZpkpZfFlBKKuGHKwyHQ6Iyd38ciy3JAjNVFpQNs/htmlembed/sheet?headers=false&gid=270422712";
 const button = document.getElementById("update");
+const vc = document.getElementById('versionCounter');
 const st1 = "window.open(\'";
 const st2 = "\',\'_blank')";
 const clickdata = st1.concat(file,st2);
 button.disabled = false;
-buttton.setAttribute("onclick",clickdata);
+button.setAttribute("onclick",clickdata);
 const vdiv = document.getElementById("version");
 const version = vdiv.className;
 const latest = 8;
