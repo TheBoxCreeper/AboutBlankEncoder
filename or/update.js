f5f2ddcb188e6b821ae7e702b16dbfb463fbc75c
TheBoxCreeper <145577997+TheBoxCreeper@users.noreply.github.com>
2025-04-23 18:04:20 -0700

    Update update.js

--- or/update.js
+++ or/update.js
@@ -1,11 +1,17 @@
 const site = "https://sites.google.com/view/terminal-games";
 const drive = "https://drive.google.com/drive/folders/1Fc5m2RE5XyS05-HWORFb7ptZXnn5fAxd";
 const file = "https://drive.usercontent.google.com/u/0/uc?id=14KXNPDGLcU9Z4B2aGslmBmtdbw9JlD2j&export=download";
-const upbutton = 1;
+const sheet = "https://docs.google.com/spreadsheets/u/0/d/1sQ9YZpkpZfFlBKKuGHKwyHQ6Iyd38ciy3JAjNVFpQNs/htmlembed/sheet?headers=false&gid=270422712";
 const button = document.getElementById("update");
+const st1 = "window.open(\'";
+const st2 = "\',\'_blank')";
+const clickdata = st1.concat(file,st2);
+button.disabled = false;
+buttton.setAttribute("onclick",clickdata);
 const vdiv = document.getElementById("version");
 const version = vdiv.className;
 const latest = 8;
 document.getElementById("TitleLink").setAttribute("href",site);
 document.getElementById("drive").setAttribute("href",drive);
+document.getElementById("UrlRef").setAttribute("src",sheet);
 console.log(version);
