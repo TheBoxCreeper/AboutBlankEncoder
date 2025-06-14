255ded6d263a2de32f92f3c3dbe1de22db2b1ebc
TheBoxCreeper <145577997+TheBoxCreeper@users.noreply.github.com>
2025-06-13 17:29:10 -0700

    Impossible Attribute

--- config.js
+++ config.js
@@ -9,7 +9,6 @@ const config = [
 	{name: 'enc-link-begin' , value: 'http://' }, /* Link Beginning, if your encoding url does not begin with a specific set of values, this will be inserted into your url. Disable this with an empty value. Acceptable values are https://, file://, http://, data:, chrome-extension://, none*/
 	{name: 'enc-default-mask-url' , value: '' }, /* Default Masking Icon Url, Use a url from the encoder's point of view. Example: 'favicons/Google Slides.png'*/
 	{name: 'enc-default-mask-title' , value: '' }, /* Default Masking Title'*/
-	{name: 'enc-acc-icon' , value: '' }, /* New Tab Icon. Use a url from the encoder's point of view. Example: 'favicons/Google Slides.png'*/
 	{name: 'enc-new-search' , value: true }, /* Make the search on the new tab page functional. Boolean*/
 	{name: 'enc-html-insertion' , value: false }, /* Enable the option to insert html. Boolean*/
 ];
